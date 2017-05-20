31917863e4a02444795ba8428190f2006eb8fee9
Max Stockner <mail@bluemaex.de>
2017-05-20 18:01:10 +0200

    iterate over directory instead a single file

--- generate-zonefile.js
+++ generate-zonefile.js
@@ -1,12 +1,12 @@
 const fs = require('fs')
+const path = require('path')
 const publicIp = require('public-ip')
 const ipv6calc = require('ipv6calc')
 const zonefile = require('dns-zonefile')
 
-const templateFile = process.argv[2]
-const zoneFile = process.argv[3]
-if (!templateFile || !zoneFile) {
-  console.error('Usage: generate-zonefile.js template.json template.zone')
+const templateDir = process.argv[2]
+if (!templateDir) {
+  console.error('Usage: generate-zonefile.js template-dir')
   process.exit(1)
 }
 
@@ -15,11 +15,17 @@ if (!templateFile || !zoneFile) {
   const ipv6 = await publicIp.v6()
   const ipv6net = ipv6calc.fromIPv6(ipv6).net
   
-  const data = fs.readFileSync(templateFile, 'utf-8')
-                 .replace(/<ipv4>/g, ipv4)
-                 .replace(/<ipv6>/g, ipv6)
-                 .replace(/<ipv6prefix>/g, ipv6net.substring(0, ipv6net.length-2))
+  fs.readdirSync(templateDir)
+    .filter(file => fs.statSync(file).isFile() && file.match(/^(?!(package)).*json$/))
+    .forEach(file => {
+      const inFile = path.join(templateDir, file)
+      
+      const data = fs.readFileSync(inFile, 'utf-8')
+                     .replace(/<ipv4>/g, ipv4)
+                     .replace(/<ipv6>/g, ipv6)
+                     .replace(/<ipv6prefix>/g, ipv6net.substring(0, ipv6net.length-2))
                      
-  const zoneData = zonefile.generate(JSON.parse(data))                 
-  fs.writeFileSync(zoneFile, zoneData, 'utf-8')
+      const outFile = inFile.replace('.json', '.forward')
+      fs.writeFileSync(outFile, zonefile.generate(JSON.parse(data)), 'utf-8')
+    })
 })()
